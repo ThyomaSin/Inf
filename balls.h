3d1fbff2bb52e94bef8d22ecd716c81460b56dda
ThyomaSin <55145123+ThyomaSin@users.noreply.github.com>
2019-09-17 19:34:15 +0300

    Add files via upload

--- balls.h
+++ balls.h
@@ -4,8 +4,8 @@ const float dt = 1;
 
 void drawBall(int x, int y, int r, int red, int green, int blue)
 {
-  int i;
-  for(i = r; i > 1; i--)
+  COLORREF color = txGetFillColor();
+  for(int i = r; i > 1; i--)
   {
     int red_i = red - red * i / r;
     int green_i = green - green * i / r;
@@ -15,25 +15,36 @@ void drawBall(int x, int y, int r, int red, int green, int blue)
     txSetFillColor(RGB(red_i,green_i,blue_i));
     txCircle(x-(r-i)/2,y-(r-i)/2.3,i);
   }
-  txSetColor(RGB(0,0,0));
-  txSetFillColor(RGB(0,0,0));
+  txSetColor(color);
+  txSetFillColor(color);
 }
 
-void moveBall(float* x, float* y, float* vx, float* vy, float dt, float r)
+void wallHit(float* x, float* y, float* vx, float* vy, float r)
 {
   if ((*y > 600 - r) or (*y < r))
     *vy = -*vy;
   if ((*x > 1000 - r) or (*x < r))
     *vx = -*vx;
+}
 
+void moveBall(float* x, float* y, float* vx, float* vy, float dt)
+{
   *x += *vx * dt;
   *y += *vy * dt;
+}
 
+void slowDown(float* vx, float* vy)
+{
   if (*vx > 0.9)
-    *vx *= 0.99;
+    *vx *= 0.97;
   if (*vy > 0.9)
-    *vy *= 0.99;
+    *vy *= 0.97;
+}
 
+void slowDownPlayer(float* vx, float* vy)
+{
+  *vy *= 0.98;
+  *vx *= 0.98;
 }
 
 void controlBall(float* vx, float* vy, float dv)
@@ -46,8 +57,6 @@ void controlBall(float* vx, float* vy, float dv)
     *vx += dv;
   if (GetAsyncKeyState(VK_DOWN))
     *vy += dv;
-  *vy *= 0.98;
-  *vx *= 0.98;
 }
 
 void drawCircle(int x, int y, int level)
@@ -63,4 +72,18 @@ void drawCircle(int x, int y, int level)
 float th_sqr(float x)
 {
   return x * x;
+}
+
+void userInt(string s)
+{
+  if (s == "level")
+    std::cout << " Choose your level '1-10'"<< std::endl;
+  if (s =="balls")
+    std::cout << "How many Balls? '1-50'" << std::endl;
+  if (s == "ball_f")
+    std::cout << "r, red, green, blue of the balls" << std::endl;
+  if (s == "player")
+    std::cout << "r, red, green, blue of the PLAYER" << std::endl;
+  if (s == "again")
+    std::cout << " play again? '1 - 0'" << std::endl;
 }
--- game v1.cpp
+++ game v1.cpp
@@ -3,91 +3,131 @@
 #include <iostream>
 
 
+struct Ball
+{
+  int r = 0;
+  int red = 0;
+  int green = 0;
+  int blue = 0;
+  float x = 0;
+  float y = 0;
+  float vx = 0;
+  float vy = 0;
+};
+
 int main()
 {
-  int i,j,r,red,green,blue,level,state;
-  state = 0;
-  float dv = 0.2;
-  int caught = 0;
-  bool ifstate = false;
-  int again = 1;
-  float n;
+  float dv = 0.4;
 
   txCreateWindow(1000,600);
-  std::cout << " Choose your level '1-10'"<< std::endl;
+
+  int level = 1;
+  userInt("level");
   std::cin >> level;
 
   drawCircle(500,300,level);
 
-  std::cout << "How many Balls? '1-20'" << std::endl;
-  std::cin >> n;
+  float n = 0;
+  do
+  {
+    userInt("balls");
+    std::cin >> n;
+  }
+  while ((n <= 0) or (n > 50));
 
-  float b_features[21][8] = {0};
-  float safe[21][8];
+  Ball b_features[51];
+  Ball safe[51];
 
-  std::cout << "r, red, green, blue of the balls" << std::endl;
+  int r,red,green,blue = {0};
+  userInt("ball_f");
   std::cin >> r >> red >> green >> blue;
 
-  for(i = 1; i <= n; i++)
+  for(int i = 1; i <= n; i++)
   {
-    b_features[i][0] = r;
-    b_features[i][1] = red;
-    b_features[i][2] = green;
-    b_features[i][3] = blue;
-    b_features[i][4] = 700;
-    b_features[i][5] = 400;
-    b_features[i][6] = (n / 2 - i) * 0.7 + 4.5;
-    b_features[i][7] = th_sqr(i - n / 2) * 0.1 + 10;
+    b_features[i].r = r;
+    b_features[i].red = red;
+    b_features[i].green = green;
+    b_features[i].blue = blue;
+    b_features[i].x = 700;
+    b_features[i].y = 400;
+    b_features[i].vx = (n / 2 - i + 0.3) * 0.7 +0.3 ;
+    b_features[i].vy = th_sqr(i - n / 2) * 0.1 + 10;
   }
 
-  std::cout << "r, red, green, blue of the PLAYER" << std::endl;
+  userInt("player");
   std::cin >> r >> red >> green >> blue;
 
-  b_features[0][0] = r;
-  b_features[0][1] = red;
-  b_features[0][2] = green;
-  b_features[0][3] = blue;
-  b_features[0][4] = 200;
-  b_features[0][5] = 200;
+  b_features[0].r = r;
+  b_features[0].red = red;
+  b_features[0].green = green;
+  b_features[0].blue = blue;
+  b_features[0].x = 200;
+  b_features[0].y = 200;
 
-  for (i = 0; i <= n; i++)
-    for (j = 0; j <= 7; j++)
-      safe[i][j] = b_features[i][j];
-
-  while (again == 1)
+  for (int i = 0; i <= n; i++)
   {
-    for (i = 0; i <= n; i++)
-      for (j = 0; j <= 7; j++)
-        b_features[i][j] = safe[i][j];
+    safe[i].r = b_features[i].r;
+    safe[i].red = b_features[i].red;
+    safe[i].green = b_features[i].green;
+    safe[i].blue = b_features[i].blue;
+    safe[i].x = b_features[i].x;
+    safe[i].y = b_features[i].y;
+    safe[i].vx = b_features[i].vx;
+    safe[i].vy = b_features[i].vy;
+  }
 
-    for (i = 0; i < 30; i++)
-      std::cout << std::endl;
-    while (state == 0)
+  int again = 1;
+
+  while (again)
+  {
+    for (int i = 0; i <= n; i++)
     {
-      for (i = 0; i <= n; i++)
-        drawBall(b_features[i][4], b_features[i][5], b_features[i][0], b_features[i][1], b_features[i][2], b_features[i][3]);
+      b_features[i].r = safe[i].r;
+      b_features[i].red = safe[i].red;
+      b_features[i].green = safe[i].green;
+      b_features[i].blue = safe[i].blue;
+      b_features[i].x = safe[i].x;
+      b_features[i].y = safe[i].y;
+      b_features[i].vx = safe[i].vx;
+      b_features[i].vy = safe[i].vy;
+    }
+
+    for (int i = 0; i < 30; i++)
+      std::cout << std::endl;
+
+    int state = 0;
+    int caught = 0;
+    bool ifstate = false;
+    while (!state)
+    {
       drawCircle(500,300,level);
+      for (int i = 0; i <= n; i++)
+      {
+        drawBall(b_features[i].x, b_features[i].y, b_features[i].r, b_features[i].red, b_features[i].green, b_features[i].blue);
+        moveBall(&b_features[i].x, &b_features[i].y, &b_features[i].vx, &b_features[i].vy, dt);
+        wallHit(&b_features[i].x, &b_features[i].y, &b_features[i].vx, &b_features[i].vy, b_features[i].r);
+        slowDown(&b_features[i].vx, &b_features[i].vy);
+      }
+      slowDownPlayer(&b_features[0].vx, &b_features[0].vy);
 
-      txSleep(4);
-      for (i = 0; i <= n; i++)
-        moveBall(&b_features[i][4],&b_features[i][5], &b_features[i][6], &b_features[i][7], dt, b_features[i][0]);
-
-      controlBall(&b_features[0][6], &b_features[0][7], dv);
-      ifstate = ifstate or (b_features[0][4] < b_features[0][0]) or (b_features[0][4] > 1000 - b_features[0][0]);
-      ifstate = ifstate or (b_features[0][5] < b_features[0][0]) or (b_features[0][5] > 600 - b_features[0][0]);
-      ifstate = ifstate or sqrt(th_sqr(b_features[0][4] - 500) + th_sqr(b_features[0][5] - 300)) < b_features[0][0] + level*20;
+      controlBall(&b_features[0].vx, &b_features[0].vy, dv);
+      ifstate = ifstate or (b_features[0].x < b_features[0].r) or (b_features[0].x > 1000 - b_features[0].r);
+      ifstate = ifstate or (b_features[0].y < b_features[0].r) or (b_features[0].y > 600 - b_features[0].r);
+      ifstate = ifstate or sqrt(th_sqr(b_features[0].x - 500) + th_sqr(b_features[0].y - 300)) < b_features[0].r + level*20;
       if (ifstate)
         state = 1;
 
-      for (i = 1; i <= n; i++)
-        if (sqrt(th_sqr(b_features[0][4] - b_features[i][4]) + th_sqr(b_features[0][5] - b_features[i][5])) < b_features[0][0] + b_features[i][0])
+      for (int i = 1; i <= n; i++)
+        if (sqrt(th_sqr(b_features[0].x - b_features[i].x) + th_sqr(b_features[0].y - b_features[i].y)) < b_features[0].r + b_features[i].r)
         {
-          b_features[i][0] = -b_features[0][0] - 1;
+          b_features[i].r = -b_features[0].r - 1;
           caught += 1;
         }
 
       if (caught == n)
         state = 2;
+
+      txSleep(4);
 
       txClear();
     }
@@ -95,11 +135,8 @@ int main()
       std::cout << "YOU LOOSE!";
     if (state == 2)
       std::cout << "YOU WON!";
-    std::cout << " play again? '1 - 0'" << std::endl;
+    userInt("again");
     std::cin >> again;
-    state = 0;
-    caught = 0;
-    ifstate = false;
   }
   return 0;
 }
