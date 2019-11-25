36738896b294e6e3010cc4d44111cba3e5c24488
ThyomaSin <artem2001sn@gmail.com>
2019-11-25 00:35:41 -0800

    Platforms

--- Platform.h
+++ Platform.h
@@ -30,13 +30,20 @@ sf::Texture* getTextureByNumber(int typeNumber, sf::Texture* textures)
 
 void Platform::generate(sf::Texture* texturePointer)
 {
-  srand(time(0));
-  int y = rand() % 640;
+  srand(time(0) + rand());
+  int y = rand() % 640;         //also need to add BRS and peresda
   int typeNumber = rand() % 5;
   this -> setPlatform(1300, y, -300, 0, getTextureByNumber(typeNumber, texturePointer))
 }
 
-
+bool checkGameObjectCollision(gameObject object1, gameObject object2)
+{
+  if ((object1.Q.x > object2.Q.x + object2.xSize) or (object1.Q.x + object1.xSize < object2.Q.x))
+    return false;
+  if ((object1.Q.y > object2.Q.y + object2.ySize) or (object1.Q.y + object1.ySize < object2.Q.y))
+    return false;
+  return true;
+}
 
  
   
